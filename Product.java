8bfe102570bd13bdd963a85067c62cb5b086d843
huashuo <1104049235@163.com>
2020-03-31 15:05:38 +0800

    分支推演rebase

--- Product.java
+++ Product.java
@@ -3,7 +3,11 @@ public class Product {
         System.out.println("I'm version 10.0!");
         System.out.println("I have a bug!");
         System.out.println("A bad feature!");
+<<<<<<< HEAD
 		System.out.print("myBranch");
         System.out.println("这是我回滚后新的分支改动");
+=======
+        System.out.println("自己分支进行推演 rebase");
+>>>>>>> 分支推演rebase
     }
 } 
