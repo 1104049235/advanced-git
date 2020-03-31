65fdf8251cb587b114bc1c8f218dfb0ab973e197
huashuo <1104049235@163.com>
2020-03-31 14:38:14 +0800

    新的分支提交

--- Product.java
+++ Product.java
@@ -3,5 +3,6 @@ public class Product {
         System.out.println("I'm version 10.0!");
         System.out.println("I have a bug!");
         System.out.println("A bad feature!");
+        System.out.println("这是我回滚后新的分支改动");
     }
 } 
