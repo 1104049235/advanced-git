b3ba2f16510fb70b38dec0273c82d409fd2663b2
huashuo <1104049235@163.com>
2020-03-31 14:45:04 +0800

    冲突解决

--- Product.java
+++ Product.java
@@ -3,10 +3,7 @@ public class Product {
         System.out.println("I'm version 10.0!");
         System.out.println("I have a bug!");
         System.out.println("A bad feature!");
-<<<<<<< HEAD
 		System.out.print("myBranch");
-=======
         System.out.println("这是我回滚后新的分支改动");
->>>>>>> myNewBranch
     }
 } 
